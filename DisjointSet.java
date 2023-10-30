a626e12c27428e55dd1bb94edc2f4f6aac6d113d
Adityakr49 <adityakumarpilot@gmail.com>
2023-10-30 10:41:46 +0530

    updated DisjointSet

--- DisjointSet.java
+++ DisjointSet.java
@@ -3,10 +3,12 @@ import java.util.*;
 import java.util.ArrayList;
 class DisjointSet{
     List<Integer> rank = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
     public DisjointSet(int n){
         for(int i=0;i<=n;i++){
             rank.add(0);
+            size.add(1);
             parent.add(i);
         }
     }
@@ -32,6 +34,19 @@ class DisjointSet{
             parent.set(ulp_v,ulp_u);
             int rankU = rank.get(ulp_u);
             rank.set(ulp_u,rankU+1);
+        }
+    }
+    public void unionBySize(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size.get(ulp_u)<size.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_v)+size.get(ulp_u))
+        }
+        else{
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v))
         }
     }
 }
